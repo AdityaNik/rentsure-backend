99aab7b6cb78c8824b38881517632c9ed9923980
AdityaNik <aadityanikam2004@gmail.com>
2025-04-19 02:28:49 +0530

    basic backend setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://adityanikam481:YQb7ocD461hqbMUt@cluster0.6clgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { dbName: 'restsureDB' })
+
+app.get('/', (req, res) => {
+    res.send('Hello....!');
+})
+
+app.listen(3000, () => {
+    console.log('backend stated on PORT 3000');
+})
